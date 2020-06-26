53c9065ee7e565c382fd030fc601ba9bfea32b49
JN Kibira <33089347+97joseph@users.noreply.github.com>
2020-06-26 21:44:13 +0300

    R

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
   const jsonfile=require('jsonfile');
   const moment=require('moment');
+  const simpleGit=require('simple-git');
 
   const FILE_PATH='./data.json';
   const DATE=moment().format();
   const data={date:DATE}
   jsonfile.writeFile(FILE_PATH,data);
+      
