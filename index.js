11adda0d442979a8c6d323d8a195b2827bc0118e
JN Kibira <33089347+97joseph@users.noreply.github.com>
2020-06-26 22:44:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 
   const FILE_PATH='./data.json';
 
-  const DATE=moment().subtract(120,'d').format();
+  const DATE=moment().subtract(15,'d').format();
   const data={date:DATE}
 
   jsonfile.writeFile(FILE_PATH,data, ()=>{ simpleGit().add([FILE_PATH]).commit(DATE, {'--date':DATE}).push();});
