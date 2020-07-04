4127844f2e48e00e397b85d612279b2c5e5b3a4e
JN Kibira <33089347+97joseph@users.noreply.github.com>
2020-07-04 05:03:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
+  //Call for jsonfile,moment amd simple-git node file
   const jsonfile=require('jsonfile');
   const moment=require('moment');
   const simpleGit=require('simple-git');
 
+//Initiate the write path
   const FILE_PATH='./data.json';
 
+  //Time from the  initial time initiate
   const DATE=moment().subtract(8,'d').format();
   const data={date:DATE}
 
+  //Try catch for counter methods
+
+  for (number=0;number<=50;number=number+1){
   jsonfile.writeFile(FILE_PATH,data, ()=>{ simpleGit().add([FILE_PATH]).commit(DATE, {'--date':DATE}).push();});
-      
+  }
   //Git commit --date=""
  
